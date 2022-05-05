cd03e9b0982cff2094794ea058c2209762eda657
NaveenB <naveen.barfa@modderstack.com>
2022-05-05 13:20:24 +0530

    changed base url

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create ({
 // baseURL : "http://localhost:4545/api/"
-baseURL : "http://http://104.154.172.11/api/"
+baseURL : "http://104.154.172.11/api/"
 })
